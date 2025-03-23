f3b66a5684ab4e11d3cd63d5fff4cf1c9a87539e
kr-rajnish <rk634107@gmail.com>
2025-03-24 00:04:47 +0530

    Error handling

--- src/app.js
+++ src/app.js
@@ -4,27 +4,74 @@ const app = express();
 
 // Error Middleware Placement – The error-handling middleware should have four parameters (err, req, res, next), but it needs to be defined after all normal middleware.
 
+// app.get(
+//   "/user",
+//   (req, res, next) => {
+//     console.log("1st response");
+//     // res.send("1at response");
+//     next();
+//   },
+//   (req, res, next) => {
+//     console.log("2nd response");
+//     // res.send("2nd response");
+//     next();
+//   },
+//   (req, res, next) => {
+//     console.log("3rd response");
+//     // res.send("3rd response");
+//     next();
+//   },
+//   (req, res, next) => {
+//     console.log("4th response");
+//     // res.send("4th response");
+//     next();
+//   },
+//   (req, res) => {
+//     console.log("5th response");
+//     res.send("5th response");
+//   }
+// );
+
+// //for sending multiple responses at single time.
+// // app.get("/user", (req, res) => {
+// //   res.write("1st response!\n");
+// //   res.write("2nd response!\n");
+// //   res.end(); // End the response
+// // });
+
+// app.use("/test", (req, res) => {
+//   res.send("Hello From Server /test!");
+// });
+
+//Error handling middleware
 app.get(
   "/user",
   (req, res, next) => {
-    console.log("1st response");
-    // res.send("1at response");
-    next();
+    try {
+      console.log("1st response");
+      // Simulate an error
+      throw new Error("Something went wrong in 1st middleware!");
+      next();
+    } catch (err) {
+      next(err); // Pass error to the error-handling middleware
+      // res.status(500).send(" Something went wrong!");
+    }
   },
   (req, res, next) => {
-    console.log("2nd response");
-    // res.send("2nd response");
-    next();
+    try {
+      console.log("2nd response");
+      next();
+    } catch (err) {
+      next(err);
+    }
   },
   (req, res, next) => {
-    console.log("3rd response");
-    // res.send("3rd response");
-    next();
-  },
-  (req, res, next) => {
-    console.log("4th response");
-    // res.send("4th response");
-    next();
+    try {
+      console.log("3rd response");
+      next();
+    } catch (err) {
+      next(err);
+    }
   },
   (req, res) => {
     console.log("5th response");
@@ -32,15 +79,10 @@ app.get(
   }
 );
 
-//for sending multiple responses at single time.
-// app.get("/user", (req, res) => {
-//   res.write("1st response!\n");
-//   res.write("2nd response!\n");
-//   res.end(); // End the response
-// });
-
-app.use("/test", (req, res) => {
-  res.send("Hello From Server /test!");
+// ✅ Proper Error-Handling Middleware (must be placed after all routes)
+app.use((err, req, res, next) => {
+  console.error("Error caught:", err.message);
+  res.status(500).send("Hello From Server! Something went wrong!"); // Send an error response
 });
 
 app.listen(3000, () => {
