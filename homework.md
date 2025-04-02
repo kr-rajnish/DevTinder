766d457fadb94f329bd5ae6457350607e8340032
kr-rajnish <rk634107@gmail.com>
2025-04-02 19:13:23 +0530

    get user by _id or by emailId or get all(feed)

--- homework.md
+++ homework.md
@@ -50,3 +50,7 @@
 - JS object vs JSON (difference).
 - Add the express.json middleware to your app.
 - Make your signup API dynamic to receive data from the end user.
+
+- User.findOne with duplicate email ids, which document(object) returned.
+- API- GET user by email.
+- API - Feed API - GET /feed - get all the users from the database.
--- src/app.js
+++ src/app.js
@@ -9,17 +9,65 @@ app.use(express.json());
 app.post("/signup", async (req, res) => {
   const user = new User(req.body);
 
-  //saving the user in db.
   try {
     //Check if user already exists
     const existingUser = await User.findOne({ emailId: req.body.emailId });
     if (existingUser) {
       return res.status(400).send("User already exists");
     }
+    //saving the user in db.
     const savedUser = await user.save();
     res.send({ message: "User saved successfully", data: savedUser });
   } catch (error) {
-    res.status(400).send(error || "Something went wrong to save the user");
+    res.status(404).send(error || "Something went wrong to save the user");
+  }
+});
+
+app.get("/user", async (req, res) => {
+  // geting user by email
+  const userEmail = req.body.emailId;
+  try {
+    //with find method returns an array
+    const user = await User.find({ emailId: userEmail }); //=> getting only the users match the emailId.
+
+    if (user.length === 0) {
+      return res.status(400).send("User not found");
+    }
+    res.send(user);
+
+    //   //with findOne method returns only one document
+    //   const user = await User.findOne({ emailId: userEmail });
+    //   if (user === null) {
+    //     return res.status(400).send("User not found");
+    //   }
+    //   res.send(user);
+  } catch (error) {
+    res
+      .status(404)
+      .send(error || "Something went wrong to get user by EmailId");
+  }
+
+  // geting user by id
+  // const userId = req.body._id;
+  // try {
+  //   const user = await User.findById(userId);
+  //   if (user === null) {
+  //     return res.status(400).send("User not found");
+  //   }
+  //   res.send(user);
+  // } catch (error) {
+  //   res.status(404).send(error || "Something went wrong to get user by Id");
+  //   // console.log(error);
+  // }
+});
+
+//Feed API - Get all the users in the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({}); //=> getting all the users
+    res.send(users);
+  } catch (error) {
+    res.status(404).send(error || "Something went wrong to get feed");
   }
 });
 
